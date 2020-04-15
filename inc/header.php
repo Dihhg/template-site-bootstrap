664b1d9c804ec4ecde3520d62cf8943014b76692
Diogo Oliveira <dihhg35@gmail.com>
2020-04-14 21:35:49 -0300

    navibar inserida

---
+++ inc/header.php
@@ -0,0 +1,28 @@
+<body>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">
+                <img src="https://4.bp.blogspot.com/-vPgAcoa_6oI/U0CA5CEqX5I/AAAAAAAAAcI/Ulfcy2RHUhQ/s1600/bola.png" alt="Logo" width="30" height="30">
+                Meu time
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto" >
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gols</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Jogadores</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Em breves novidades</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
